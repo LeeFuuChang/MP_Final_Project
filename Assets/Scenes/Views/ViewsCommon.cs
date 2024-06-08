bf70044c69cfe2f6fa0f8ceca27bd523634e2f41
leefuuchang <a0962014248gg@gmail.com>
2024-06-08 23:09:03 +0800

    add 360 pic

--- Assets/Scenes/Views/ViewsCommon.cs
+++ Assets/Scenes/Views/ViewsCommon.cs
@@ -7,6 +7,39 @@ using UnityEngine.UI;
 
 public class ViewsCommon : MonoBehaviour
 {
+    private GameObject infoLayer;
+    private GameObject continueButton;
+    private GameObject exitButton;
+    
+	void Start()
+    {
+        continueButton = GameObject.Find("continueBtn");
+		Button continueBtn = continueButton.GetComponent<Button>();
+        continueBtn.onClick.AddListener(this.ContinueButtonClicked);
+
+        exitButton = GameObject.Find("exitBtn");
+		Button exitBtn = exitButton.GetComponent<Button>();
+        exitBtn.onClick.AddListener(this.ExitButtonClicked);
+
+        infoLayer = GameObject.Find("InfoLayer");
+        infoLayer.SetActive(!GameObject.Find("360"));
+    }
+
+    void LateUpdate() 
+    {
+        if(!GameObject.Find("360")) return;
+
+        int turnSpeedMouse = 1000;
+
+        Transform camera = GameObject.Find("Main Camera").GetComponent<Transform>();
+        camera.Rotate(new Vector3(Input.GetAxis("Mouse Y"),-Input.GetAxis("Mouse X"), 0)*Time.deltaTime*turnSpeedMouse);
+    }
+
+    public void ContinueButtonClicked()
+    {
+        infoLayer.SetActive(true);
+    }
+
     public void ExitButtonClicked()
     {
         SceneManager.LoadScene("Game");
